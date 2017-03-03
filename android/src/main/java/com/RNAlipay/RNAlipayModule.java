036c37de7cfef7f52e5da0a7e16afcffcbb6bf16
cmk <mingkang1993@qq.com>
2017-03-03 14:35:57 +0800

    init

---
+++ android/src/main/java/com/RNAlipay/RNAlipayModule.java
@@ -0,0 +1,68 @@
+package com.alipay;
+
+import android.text.TextUtils;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import com.alipay.sdk.app.PayTask;
+
+public class RNAlipayModule extends ReactContextBaseJavaModule {
+	private final ReactApplicationContext mReactContext;
+
+	public RNAlipayModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+		mReactContext = reactContext;
+  	}
+  	
+	@Override
+  	public String getName() {
+    	return "RNAlipay";
+  	}
+
+  	private void sendEvent(ReactContext reactContext,
+                           String eventName,
+						   Boolean params) {
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+         .emit(eventName, params);
+    }
+
+
+  	@ReactMethod
+  	public void pay(ReadableMap options, Promise promise) {
+
+        String payInfo = options.getString("payInfo");
+
+		if (TextUtils.isEmpty(payInfo)) {
+
+		    promise.reject("需要配置payInfo,传给支付宝的参数");
+
+			return;
+		}
+
+        System.out.println(payInfo);
+
+		PayTask alipay = new PayTask(getCurrentActivity());
+		String result = alipay.pay(payInfo);
+		//cb.invoke(result);
+		promise.resolve(result);
+
+        String resultCode = "0";
+        String[] split = result.split(";");
+        for (int i = 0; i < split.length; ++i) {
+            String resultInfo = split[i];
+            if (resultInfo.contains("resultStatus")) {
+                String[] results = resultInfo.replace("{", "").replace("}", "").split("=");
+                if (results.length > 1) {
+                    resultCode = results[1];
+                }
+            }
+        }
+        sendEvent(mReactContext, "rnAlipayCallback", resultCode.equals("9000") ? true : false);
+    }
+}
---
+++ ios/RNAlipay/RNAlipay.m
@@ -0,0 +1,91 @@
+#import "RNAlipay.h"
+#import "Order.h"
+#import "DataSigner.h"
+@interface RNAlipay()
+@property (nonatomic,strong)NSURL *resultUrl;
+
+@end
+
+static RNAlipay * instancea = nil;
+@implementation RNAlipay
+
+RCT_EXPORT_MODULE();
+
+@synthesize bridge = _bridge;
+
++ (instancetype)shareInstance {
+    @synchronized(self) {
+        if (!instancea) {
+            instancea = [[self alloc] init];
+        }
+    }
+    return instancea;
+}
+
++ (instancetype)allocWithZone:(NSZone *)zone {
+    @synchronized(self) {
+        if (!instancea) {
+            instancea = [super allocWithZone:zone];
+        }
+    }
+    return instancea;
+}
+
+- (instancetype)copyWithZone:(NSZone *)zone {
+    return self;
+}
+
+
+
+- (void)processOrderWithPaymentResult:(NSURL *)resultUrl {
+    self.resultUrl = resultUrl;
+    
+    [[AlipaySDK defaultService] processAuthResult:resultUrl standbyCallback:^(NSDictionary *resultDic) {
+//        NSLog(@"reslut = %@",resultDic);
+        if ([resultDic count] != 0) {
+            NSLog(@"orderString = %@", @"支付成功啦啦啦啦！");
+            [instancea.bridge.eventDispatcher sendAppEventWithName:@"rnAlipayCallback"
+                                                             body:@true];
+        }else{
+            NSLog(@"支付失败");
+            [instancea.bridge.eventDispatcher sendAppEventWithName:@"rnAlipayCallback"
+                                                                         body:@false];
+        }
+    }];
+
+
+}
+
+
+
+
+RCT_REMAP_METHOD(pay, options:(NSDictionary *)options
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    
+    NSString *appScheme = [options objectForKey:@"appSchemeIOS"];
+
+    //将签名成功字符串格式化为订单字符串,请严格按照该格式
+    NSString *orderString = [options objectForKey:@"payInfo"];
+    if (orderString != nil) {
+
+//        NSLog(@"orderString = %@", orderString);
+
+
+        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+//            NSLog(@"reslut = %@",resultDic);
+//
+//            NSLog(@"orderString = %@", @"支付成功啦啦啦啦！");
+//            resolve(@"支付成功!");
+        }];
+        
+        return;
+    }
+
+    NSError *error = nil;
+    reject(@"支付失败", @"参数错误", error);
+
+}
+
+@end
